6e8a2a5730b94c8bad0eb6cf1975b1719b3ff016
“enamul58” <enamullimon@gmail.com>
2024-11-06 13:27:04 +0600

    addtion of fraction number

---
+++ 3 Data Types Variables & Input/Input/src/DoubleNumberInput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class DoubleNumberInput {
+    public static void main(String[] args){
+        Scanner DoubleNumber = new Scanner(System.in);
+        System.out.println("Please enter double numeric value");
+        double DoubleNumberNum1 = DoubleNumber.nextDouble();
+        System.out.println("please enter double numeric value");
+        double DoubleNumberNum2 = DoubleNumber.nextDouble();
+        double result = DoubleNumberNum1 + DoubleNumberNum2;
+        System.out.println("addition result of fraction number:" + result);
+
+    }
+}
